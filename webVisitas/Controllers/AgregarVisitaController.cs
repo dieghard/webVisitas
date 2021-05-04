6b7927924a93b411ebedf95d4aecd78b995e2805
Diego Markiewicz <dieghard@gmail.com>
2021-05-04 13:58:45 -0300

    Agregar archivos de proyecto.

---
+++ webVisitas/Controllers/AgregarVisitaController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace webVisitas.Controllers
+{
+    public class AgregarVisitaController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public AgregarVisitaController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+
+            string webRootPath = _webHostEnvironment.WebRootPath;
+
+            try
+            {
+
+                string autor_observaciones = HttpContext.Request.Form["FirstName"] + "\nComentario: " + HttpContext.Request.Form["observaciones"];
+                string lineaSeparadora = "\n_____________________________________________________________________________________________________________\n";
+                string fecha = System.DateTime.Now.ToString("yyyyMMdd");
+                string hora = System.DateTime.Now.ToString("HH:mm:ss");
+
+                System.IO.StreamWriter archivo = new StreamWriter(Path.Combine(webRootPath, "archivo.txt") , true);
+                archivo.WriteLine("\nFecha: " + fecha + "\nHora: " + hora + "\nAutor: " + autor_observaciones + lineaSeparadora);
+                archivo.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+
+            return View();
+        }
+
+    }
+}
---
+++ webVisitas/Controllers/LeerController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace webVisitas.Controllers
+{
+    public class LeerController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public LeerController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            string webRootPath = _webHostEnvironment.WebRootPath;
+            string data;
+
+            try
+            {
+             
+                string path = Path.Combine(webRootPath, "archivo.txt");
+                data = System.IO.File.ReadAllText(path, Encoding.UTF8);
+            }
+            catch (Exception e)
+            {
+                data = "Exception: " + e.Message;
+            }
+
+            // Por las dudas, ajusto el CrLf al de Enironment
+            data = data.Replace("\n", Environment.NewLine);
+            return View(null, data);
+        }
+    }
+}
